6d14730c394de1d2fcf7f52148e99a1006718cd1
tarima <tatsuya.arima@nts.ricoh.co.jp>
2012-11-27 08:44:52 +0900

    random flash
    1.random flash
    2.number sorting

--- GridButton.py
+++ GridButton.py
@@ -7,28 +7,32 @@ Created on 2012/11/15
 '''
 from Tkinter import *
 import threading
+import random
 
 root = Tk()
 root.title('おもしろアプリ')
 
-column_data = (0, 0, 0, 1, 1, 1, 2, 2, 2)
-row_data = (0, 1, 2, 0, 1, 2, 0, 1, 2)
+column_data = (0, 1, 2, 0, 1, 2, 0, 1, 2)
+row_data = (0, 0, 0, 1, 1, 1, 2, 2, 2)
+buttonList = []
 
 for x in range(9):
-    b = Button(root, text = '%d' % (x + 1), font=('ＭＳ 明朝', 20), bg = '#00FFFF', activebackground = '#00FF00')
-    b.grid(column = column_data[x], row = row_data[x], sticky = 'nsew')
+    buttonList.append(Button(root, text = '%d' % (x + 1), font=('ＭＳ 明朝', 20), bg = '#00FFFF', activebackground = '#00FF00'))
+	buttonList[x].grid(column = column_data[x], row = row_data[x], sticky = 'nsew')
 
 for x in range(3):
-    root.grid_columnconfigure(x, weight = 1)
-    root.grid_rowconfigure(x, weight = 1)
+	root.grid_columnconfigure(x, weight = 1)
+	root.grid_rowconfigure(x, weight = 1)
 
 def buttonflash():
-    b.flash()
-    ButtonFlashTimer()
+	#b.flash()
+	#random.seed(1)
+	buttonList[random.randint(0,8)].flash()
+	ButtonFlashTimer()
 
 def ButtonFlashTimer():
-    t = threading.Timer(1, buttonflash)
-    t.start()
+	t = threading.Timer(1, buttonflash)
+	t.start()
 
 ButtonFlashTimer()
 
