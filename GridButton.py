cae8e66878e68d39629392adc9ecb1ca11bf8695
tarima <tatsuya.arima@nts.ricoh.co.jp>
2012-11-27 13:16:51 +0900

    Hit Judgment
    if you push button when lightning, 'OK' message appear.

--- GridButton.py
+++ GridButton.py
@@ -8,32 +8,52 @@ Created on 2012/11/15
 from Tkinter import *
 import threading
 import random
+import tkMessageBox
 
 root = Tk()
 root.title('おもしろアプリ')
+BUTTONLIST = []
+RANDOMNUM = 0
 
-column_data = (0, 1, 2, 0, 1, 2, 0, 1, 2)
-row_data = (0, 0, 0, 1, 1, 1, 2, 2, 2)
-buttonList = []
+class ButtonPush:
+	def __init__(self, buttonNum):
+		self.buttonNum = buttonNum
 
-for x in range(9):
-    buttonList.append(Button(root, text = '%d' % (x + 1), font=('ＭＳ 明朝', 20), bg = '#00FFFF', activebackground = '#00FF00'))
-	buttonList[x].grid(column = column_data[x], row = row_data[x], sticky = 'nsew')
+	def __call__(self, event=None):
+		if RANDOMNUM == self.buttonNum:
+			tkMessageBox.showinfo('showinfo', 'OK!')
+		else:
+			tkMessageBox.showerror('showerror', 'ERROR!')
 
-for x in range(3):
-	root.grid_columnconfigure(x, weight = 1)
-	root.grid_rowconfigure(x, weight = 1)
+class ButtonMake:
+	column_data = (0, 1, 2, 0, 1, 2, 0, 1, 2)
+	row_data = (0, 0, 0, 1, 1, 1, 2, 2, 2)
 
-def buttonflash():
-	#b.flash()
-	#random.seed(1)
-	buttonList[random.randint(0,8)].flash()
-	ButtonFlashTimer()
+	for x in range(9):
+		BUTTONLIST.append(Button(root, text = '%d' % (x + 1), font=('ＭＳ 明朝', 20),
+			bg = '#00FFFF', activebackground = '#00FF00', command=ButtonPush(x)))
+		BUTTONLIST[x].grid(column = column_data[x], row = row_data[x], sticky = 'nsew')
 
-def ButtonFlashTimer():
-	t = threading.Timer(1, buttonflash)
-	t.start()
+	for x in range(3):
+		root.grid_columnconfigure(x, weight = 1)
+		root.grid_rowconfigure(x, weight = 1)
 
-ButtonFlashTimer()
+class ButtonFlash:
+	def __init__(self):
+		self.ButtonFlashTimer()
 
-root.mainloop()
+	def buttonflash(self):
+		global RANDOMNUM
+		RANDOMNUM = random.randint(0,8)
+		BUTTONLIST[RANDOMNUM].flash()
+		self.ButtonFlashTimer()
+
+	def ButtonFlashTimer(self):
+		t = threading.Timer(1, self.buttonflash)
+		t.start()
+
+##------------------------------------------------
+if __name__ == '__main__':
+	ButtonMake()
+	ButtonFlash()
+	root.mainloop()
